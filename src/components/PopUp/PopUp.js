37d79d4af40b76434659e4aa0ccb3621c8e0b4c8
johnBWeiss <yoniw7@gmail.com>
2022-08-09 17:28:31 +0300

    pop height under 400px and type attribute for video tag

--- src/components/PopUp/PopUp.js
+++ src/components/PopUp/PopUp.js
@@ -12,11 +12,10 @@ const PopUp = (popUP) => {
         <div className='popUp' onClick={showPopUpHandler}>
             <div className='popUpInnerContainer'>
                 <img className='X' src={X} alt='X' />
-
                 <div className='popUpTitle'>{content.title}</div>
                 <div className='upperPopUpContent'>
                     <div className='playerContainer'>
-                        <video width="100%" height="100%" src={content.src} autoPlay muted playsInline>
+                        <video width="100%" height="100%" src={content.src} autoPlay muted playsInline type='video/mp4'>
                         </video>
 
                     </div>
